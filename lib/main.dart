d97894340f4b223557d20712cc8aeffdaf54fb79
Harsh <harsh.sharma@goextramile.in>
2024-12-26 13:23:04 +0530

    firebase config (#9)

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,12 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:helpnest/core/config/firebase_options.dart';
 import 'package:helpnest/core/config/theme.dart';
 import 'package:helpnest/features/home/presentation/pages/home_controller.dart';
 
-void main() {
+Future<void> main() async {
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   runApp(const App());
 }
 
